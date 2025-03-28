6435d1e8f71b5b361bc862f9a2ddff34c608e752
Daniel D <22680274+danieldreke@users.noreply.github.com>
2025-03-29 00:31:45 +0100

    Fix case of navbar title from Graphenos to GrapheneOS

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -2,9 +2,16 @@ document.title = document.title.split(' ')
     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ');
 
-const spans = document.querySelectorAll('span.md-ellipsis');
-spans.forEach(span => {
-    if (span.innerHTML.toLowerCase().includes('grapheneos')) {
-        span.innerHTML = span.innerHTML.replace(/grapheneos/i, 'GrapheneOS');
+const navitems = document.querySelectorAll('span.md-ellipsis');
+navitems.forEach(navitem => {
+    if (navitem.innerHTML.toLowerCase().includes('grapheneos')) {
+        navitem.innerHTML = navitem.innerHTML.replace(/grapheneos/i, 'GrapheneOS');
+    }
+});
+
+const navtitles = document.querySelectorAll('label.md-nav__title');
+navtitles.forEach(navtitle => {
+    if (navtitle.innerHTML.toLowerCase().includes('grapheneos')) {
+        navtitle.innerHTML = navtitle.innerHTML.replace(/grapheneos/i, 'GrapheneOS');
     }
 });
