af860689a1854cab444488a394ac3b5e926d64ce
Daniel Dreke <22680274+danieldreke@users.noreply.github.com>
2025-05-19 17:22:13 +0200

    Update script.js

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -6,6 +6,7 @@ const navitems = document.querySelectorAll('span.md-ellipsis');
 navitems.forEach(navitem => {
     if (navitem.innerHTML.toLowerCase().includes('grapheneos')) {
         navitem.innerHTML = navitem.innerHTML.replace(/grapheneos/i, 'GrapheneOS');
+        navitem.innerHTML = navitem.innerHTML.replace(/vpn/i, 'VPN');
     }
 });
 
@@ -13,5 +14,6 @@ const navtitles = document.querySelectorAll('label.md-nav__title');
 navtitles.forEach(navtitle => {
     if (navtitle.innerHTML.toLowerCase().includes('grapheneos')) {
         navtitle.innerHTML = navtitle.innerHTML.replace(/grapheneos/i, 'GrapheneOS');
+        navtitle.innerHTML = navtitle.innerHTML.replace(/vpn/i, 'VPN');
     }
 });
